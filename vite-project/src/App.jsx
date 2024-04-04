ec2d283b27ec15d140620d770b8d7342f964b5e8
Jonathan Vides <jonathan@Jonathans-Air.attlocal.net>
2024-04-03 19:16:06 -0500

    Rendered data and added styling

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,21 +1,38 @@
 import './App.css'
 import { useState } from 'react'
-import {puppyList} from './data.js'
+import { puppyList } from './data.js'
 
 function App() {
     const [puppies, setPuppies] = useState(puppyList);
+    const [featPupId, setFeatPupId] = useState(null);
     console.log("puppyList: ", puppyList);
 
+    const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+    console.log(featuredPup);
+
     return (
-    <>
-    <div className="App">
-        {
-            puppies.map((puppy) => {
-                return <p key={puppy.id}>{puppy.name}</p>;
-            }) 
-        }
-   </div>
-   </>
+        <>
+            <div className="App">
+                {puppies.map((puppy) => {
+                    return (
+                        <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>
+                            {puppy.name}
+                        </p>
+                    );
+                })
+                }
+
+                { featPupId && (
+                    <div className='puppyDetails'>
+                        <h2 className='puppyNames'>{featuredPup.name}</h2> 
+                        <ul className='puppyList'>
+                            <li className='puppyAge'>Age: {featuredPup.age}</li>
+                            <li className='puppyEmail'>Email: {featuredPup.email}</li>
+                        </ul>
+                    </div>
+                )}
+            </div>
+        </>
     );
 }
 
